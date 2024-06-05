210b49b1b714452fdd024be6e311cb53aab73a35
jessvasq <jessnsq.codes@gmail.com>
2024-06-05 14:42:37 -0400

    HTML resume basic layout

---
+++ Assignments/BasicHTMLLayout/src/main/java/com/nvz/basichtmllayout/BasicHtmlLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.basichtmllayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicHtmlLayoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicHtmlLayoutApplication.class, args);
+    }
+
+}
---
+++ Assignments/BasicHTMLLayout/src/test/java/com/nvz/basichtmllayout/BasicHtmlLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.basichtmllayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicHtmlLayoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
