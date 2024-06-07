96d5afd7c0a97e121a7d3540d31e512599aa9226
jessvasq <jessnsq.codes@gmail.com>
2024-06-07 11:33:48 -0400

    login page

---
+++ Assignments/CSS_Properties_Practice/src/main/java/com/nvz/css_properties_practice/CssPropertiesPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.css_properties_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssPropertiesPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CssPropertiesPracticeApplication.class, args);
+    }
+
+}
