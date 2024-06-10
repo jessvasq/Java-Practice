8f9e55501e58bf4c0c09edbc319f97f66a207e1b
jessvasq <jessnsq.codes@gmail.com>
2024-06-10 16:10:41 -0400

    Resume template

---
+++ Assignments/PA307_4_1_Resume_using_HTMLCSS/src/main/java/com/nvz/pa307_4_1_resume_using_htmlcss/Pa30741ResumeUsingHtmlcssApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.pa307_4_1_resume_using_htmlcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa30741ResumeUsingHtmlcssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pa30741ResumeUsingHtmlcssApplication.class, args);
+    }
+
+}
---
+++ Assignments/PA307_4_1_Resume_using_HTMLCSS/src/test/java/com/nvz/pa307_4_1_resume_using_htmlcss/Pa30741ResumeUsingHtmlcssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.pa307_4_1_resume_using_htmlcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa30741ResumeUsingHtmlcssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
