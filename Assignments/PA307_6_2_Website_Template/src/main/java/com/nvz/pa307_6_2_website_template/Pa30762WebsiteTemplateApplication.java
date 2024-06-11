5b4472dfff47cb183857d6a862f091af35ffb7a8
jessvasq <jessnsq.codes@gmail.com>
2024-06-10 22:03:58 -0400

    user page using bootstrap

---
+++ Assignments/PA307_6_2_Website_Template/src/main/java/com/nvz/pa307_6_2_website_template/Pa30762WebsiteTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.pa307_6_2_website_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa30762WebsiteTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pa30762WebsiteTemplateApplication.class, args);
+    }
+
+}
---
+++ Assignments/PA307_6_2_Website_Template/src/test/java/com/nvz/pa307_6_2_website_template/Pa30762WebsiteTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.pa307_6_2_website_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa30762WebsiteTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
