643bbc44798ac0289dc677a2c0e867d7494f6878
jessvasq <jessnsq.codes@gmail.com>
2024-06-29 23:30:51 -0400

    RequestParam Annotation

---
+++ Labs/GL309_5_5_SP7/src/main/java/com/nvz/gl309_5_5_sp7/Gl30955Sp7Application.java
@@ -0,0 +1,13 @@
+package com.nvz.gl309_5_5_sp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gl30955Sp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gl30955Sp7Application.class, args);
+    }
+
+}
---
+++ Labs/GL309_5_5_SP7/src/test/java/com/nvz/gl309_5_5_sp7/Gl30955Sp7Application.java
@@ -0,0 +1,16 @@
+package com.nvz.gl309_5_5_sp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootApplication
+public class Gl30955sp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gl30955sp7Application.class, args);
+    }
+
+}
