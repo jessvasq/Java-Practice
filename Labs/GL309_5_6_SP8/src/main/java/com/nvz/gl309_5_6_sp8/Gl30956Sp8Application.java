2bd672ad17610790317d120e144f5d8a506af41e
jessvasq <jessnsq.codes@gmail.com>
2024-07-03 22:07:11 -0400

    PathVariable annotation

---
+++ Labs/GL309_5_6_SP8/src/main/java/com/nvz/gl309_5_6_sp8/Gl30956Sp8Application.java
@@ -0,0 +1,13 @@
+package com.nvz.gl309_5_6_sp8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gl30956Sp8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gl30956Sp8Application.class, args);
+    }
+
+}
---
+++ Labs/GL309_5_6_SP8/src/test/java/com/nvz/gl309_5_6_sp8/Gl30956Sp8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.gl309_5_6_sp8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gl30956Sp8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
