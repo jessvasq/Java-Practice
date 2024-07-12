894e51f15621226c03db1412ce5f8853bf443a42
jessvasq <jessnsq.codes@gmail.com>
2024-07-12 15:36:44 -0400

    Spring data rest crud api

---
+++ Labs/GL310_1_1_SP18_SpringData_CRUD_API/src/main/java/com/nvz/gl309_7_1_sp12_jpa_crud_api/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.nvz.gl309_7_1_sp12_jpa_crud_api.repository;
+
+import com.nvz.gl309_7_1_sp12_jpa_crud_api.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+//We are using JpaRepository’s methods: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()
+@RepositoryRestResource(path="learningtutorials")
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    // custom method
+    List<Tutorial> findByPublished(boolean published);
+}
